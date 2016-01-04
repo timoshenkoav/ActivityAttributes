a8e8aab7aac5b888f6e3bb34a8a46d15d2b939f4
Alexandr Timoshenko <thick.tav@gmail.com>
2016-01-04 19:08:26 +0100

    read custom attributes in activity

--- app/src/main/java/com/tunebrains/activityattributes/MainActivity.java
+++ app/src/main/java/com/tunebrains/activityattributes/MainActivity.java
@@ -1,7 +1,9 @@
 package com.tunebrains.activityattributes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = (TextView) findViewById(R.id.text);
+        TypedValue lTypedText = new TypedValue();
+
+        getTheme().resolveAttribute(R.attr.TitleText, lTypedText, true);
+        //check if we have value set
+        if (lTypedText.resourceId != 0) {
+            String title = getResources().getString(lTypedText.resourceId);
+            text.setText(title);
+        }
     }
 }
